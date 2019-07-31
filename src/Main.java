0a33bae267d08d60b51c88fd27b92d5960057917
Nilay Patel <nilay.patel@ada.ac.uk>
2019-07-31 14:33:49 +0100

    implemented a file reader to read source code file.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        String line = "";
+        String data = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("src/test.venta"));
+            while (line != null) {
+                data += line;
+                line = bufferedReader.readLine();
 
-
+            }
+            System.out.println(data);
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Unable to read the file.");
+        }
     }
 }

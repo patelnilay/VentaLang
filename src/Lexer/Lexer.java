ddbcc521a5d02ea96ce7aa665f1f811ecb4757a7
Nilay Patel <nilay.patel@ada.ac.uk>
2019-07-31 14:37:04 +0100

    created Lexer package and Lexer class. Set up the lexer with a test output

---
+++ src/Lexer/Lexer.java
@@ -0,0 +1,14 @@
+package Lexer;
+
+public class Lexer {
+    public static void main(String[] args) {
+        String sourceCode = "";
+
+    }
+
+    public String tokenise(){
+        System.out.println("YAhooo");
+        return null;
+    }
+
+}

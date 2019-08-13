fbf038ee225011dcb54b9eb7db81be23ae6ecd19
Nilay Patel <nilay.patel@ada.ac.uk>
2019-08-13 15:09:43 +0100

    changed return type after realising that the method didn't what I expected. Changed return type to List of Lists which is what I expected/wanted

--- src/Lexer/Lexer.java
+++ src/Lexer/Lexer.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 
 public class Lexer {
-    private static List<String[]> tokens = new ArrayList<>();
+    private static List<List<String>> tokens = new ArrayList<>();
 
-    public List tokenise(String sourceCode) {
+    public List<List<String>> tokenise(String sourceCode) {
         String[] addToTokens;
         String[] SplitSourceCode;
         SplitSourceCode = sourceCode.split(" ");
@@ -25,31 +25,31 @@ public class Lexer {
             // checks is currentWord is "var" and create a "VAR DECLARATION" token for it
             if (currentWord.equals("var")) {
                 addToTokens = new String[]{"VAR DECLARATION", currentWord};
-                tokens.add(addToTokens);
+                tokens.add(Arrays.asList(addToTokens));
                 // Using regex, this will recognise a word and create an IDENTIFIER token for it.
             } else if (currentWord.matches("[a-zA-Z]+")) {
                 addToTokens = new String[]{"IDENTIFIER", currentWord};
-                tokens.add(addToTokens);
+                tokens.add(Arrays.asList(addToTokens));
             } else if (currentWord.matches("[0-9]+")) {
                 addToTokens = new String[]{"INTEGER", currentWord};
-                tokens.add(addToTokens);
+                tokens.add(Arrays.asList(addToTokens));
             } else if (Arrays.asList(operatorsArray).contains(currentWord)) {
                 addToTokens = new String[]{"OPERATOR", currentWord};
-                tokens.add(addToTokens);
+                tokens.add(Arrays.asList(addToTokens));
                 // This will recognise an a semicolon [;] and create an SYMBOL token for it.
             } else if (currentWord.contains(";")) {
                 if (currentWordWithoutSemiColon.matches("[a-zA-Z]+")) {
                     addToTokens = new String[]{"IDENTIFIER", currentWordWithoutSemiColon};
-                    tokens.add(addToTokens);
+                    tokens.add(Arrays.asList(addToTokens));
                     addToTokens = new String[]{"SYMBOL", lastChar};
-                    tokens.add(addToTokens);
+                    tokens.add(Arrays.asList(addToTokens));
                     // Using regex, this will recognise an integer and create a INTEGER token for it.
                 }
                 if (currentWordWithoutSemiColon.matches("[0-9]+")) {
                     addToTokens = new String[]{"INTEGER", currentWordWithoutSemiColon};
-                    tokens.add(addToTokens);
+                    tokens.add(Arrays.asList(addToTokens));
                     addToTokens = new String[]{"SYMBOL", lastChar};
-                    tokens.add(addToTokens);
+                    tokens.add(Arrays.asList(addToTokens));
                     // This will recognise an operator and create a OPERATOR token for it.
                 }
             }
